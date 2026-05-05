a92f103bd10177f091325ce9a48bf19c3336725a
Lucas Lamar <lucass.lamar@gmail.com>
2026-05-05 18:41:57 -0300

    CHORE: CONFIGS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // O nome deve ser EXATAMENTE o nome do seu repositório no GitHub
-  base: '/Riichi-Manager/', 
+  base: './',
 })
